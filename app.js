f8d1dda961a59bc084c4ec39054231a3754e04fa
martnga <mtnjoroge@gmail.com>
2016-01-26 13:36:28 +0300

    Views

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 
-
+// Handle File Uploads
+app.use(multer({dest:'./uploads'}).single('photo'));
         
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
